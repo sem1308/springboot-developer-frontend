d41f3a27d1c245d173461f0727cd8a43137ceced
한수한 <hshhan1009@naver.com>
2024-05-06 22:58:01 +0900

    feat : user 회원가입, 로그인 페이지 추가

---
+++ src/views/user/UserLoginView.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+import { login } from '@/apis/user-api';
+
+const router = useRouter();
+
+const user = ref({
+    email: "",
+    password: ""
+})
+
+const onLogin = async () => {
+    const response = await login(user.value);
+
+    if (response.status == 200) {
+        router.push({ name: "/" });
+    }
+}
+
+</script>
+
+<template>
+    <div class="gradient-custom">
+        <section class="d-flex vh-100">
+            <div class="container-fluid row justify-content-center align-content-center">
+                <div class="card bg-dark" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <h2 class="text-white">LOGIN</h2>
+                        <p class="text-white-50 mt-2 mb-5">서비스를 사용하려면 로그인을 해주세요!</p>
+
+                        <div class="mb-2">
+                            <form @submit.prevent="onLogin()">
+                                <div class="mb-3">
+                                    <label class="form-label text-white">Email address</label>
+                                    <input type="email" class="form-control" name="email">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label text-white">Password</label>
+                                    <input type="password" class="form-control" name="password">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+
+                            <button type="button" class="btn btn-secondary mt-3"
+                                onclick="location.href='/users/signup'">회원가입</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+.gradient-custom {
+    background: linear-gradient(to right, rgba(254, 238, 229, 1), rgba(229, 193, 197, 1))
+}
+</style>
---
+++ src/views/user/UserSignUpView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+import { createUser } from '@/apis/user-api';
+
+const router = useRouter();
+
+const user = ref({
+    email: "",
+    password: ""
+})
+
+const onCreateUser = async () => {
+    const id = await createUser(user.value);
+
+    if (id) {
+        router.push({ name: "login" });
+    }
+}
+
+</script>
+
+<template>
+    <div class="gradient-custom">
+        <section class="d-flex vh-100">
+            <div class="container-fluid row justify-content-center align-content-center">
+                <div class="card bg-dark" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+                        <h2 class="text-white">SIGN UP</h2>
+                        <p class="text-white-50 mt-2 mb-5">서비스 사용을 위한 회원 가입</p>
+
+                        <div class="mb-2">
+                            <form @submit.prevent="onCreateUser()">
+                                <!-- 토큰을 추가하여 CSRF 공격 방지 -->
+                                <input type="hidden" />
+                                <div class="mb-3">
+                                    <label class="form-label text-white">Email address</label>
+                                    <input type="email" class="form-control" v-model="user.email" />
+                                    <div class="mb-3">
+                                        <label class="form-label text-white">Password</label>
+                                        <input type="password" class="form-control" v-model="user.password">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<style scoped>
+.gradient-custom {
+    background: linear-gradient(to right, rgba(254, 238, 229, 1), rgba(229, 193, 197, 1))
+}
+</style>
