af819b1376f5179defc39f404dc26fae77996c35
한수한 <hshhan0221@naver.com>
2024-06-07 16:45:41 +0900

    feat : 인증 성공시 작동하는 handler 추가

---
+++ src/components/AuthHandler.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router';
+import { searchParam } from '@/js/token'
+
+const router = useRouter();
+
+onMounted(()=>{
+    // token 받아오기 (로그인 확인)
+    const token = searchParam('token') 
+    console.log("?????")
+    if (token) {
+        localStorage.setItem("access_token", token);
+        console.log("oauth success")
+        router.push({name:'list'})
+    }else{
+        console.log("oauth failed")
+        router.push({name:'login'})
+    }
+})
+
+</script>
