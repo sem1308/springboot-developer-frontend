25fc5f8bbfd9c8cadccc9ed02b24addea614cea5
한수한 <hshhan1009@naver.com>
2024-06-09 14:51:43 +0900

    feat : 로그인 여부에 따른 로그인, 로그아웃 표시

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue';
+
+const token = ref(localStorage.getItem('access_token'))
 
 </script>
 
@@ -9,6 +12,8 @@
         </h1>
         <h4 class="mb-3">블로그에 오신 것을 환영합니다.</h4>
     </div>
+    <router-link v-show="!token" class="btn btn-primary text-white" :to="{ name: 'login' }">로그인</router-link>
+    <button v-show="token" class="btn btn-secondary text-white">로그아웃</button>
 </template>
 
 <style scoped>
